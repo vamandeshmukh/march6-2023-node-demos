6ef400ddd6b7a4e317c488e2b00a5fcea189059e
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-03-21 10:09:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# march6-2023-node-demos
+ NodeJS demos for Deloitte BLR & HYD trainees 6 Mar 2023
---
+++ script.js
@@ -0,0 +1,9 @@
+
+// JS 
+// JS engine 
+
+// chrome - v8
+// edge - chakra 
+
+console.log('Hello world!');
+ 
