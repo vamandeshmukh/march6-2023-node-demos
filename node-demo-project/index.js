a96d78dbc14a2399e9ca3f1e2eb24e4118810f46
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-03-21 12:20:53 +0530

    node-fundas

--- node-demo-project/index.js
+++ node-demo-project/index.js
@@ -1,3 +1,23 @@
 
-console.log('Hello world!');
+const express = require('express');
+const app = express();
 
+app.get('/', function (req, res) {
+    res.send('Hello World')
+});
+
+app.listen(3000);
+
+
+
+
+// // core java
+// // adv java - jdbc, servlet
+
+// // white rice
+// // lemon rice
+
+// console.log('Hello world!');
+
+// // M E R N stack
+// // M E A N stack 
